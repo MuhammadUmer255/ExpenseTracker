a4fa06e37e2582634f5cb55704ca89a9fc9a3b2e
Muhammad Umer <muhammad.umer25052@gmail.com>
2026-07-21 22:54:35 +0500

    Added login functionality

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,15 +1,23 @@
 const express = require("express");
+const cors = require("cors");
+const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 
+connectDB();
+
+app.use(cors());
 app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({ message: "Expense Tracker API running" });
+});
+
+app.use("/api/auth", authRoutes);
 
 const PORT = 5000;
 
-app.get("/", (req, res) => {
-    res.send("Expense Tracker API is Running...");
-});
-
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
