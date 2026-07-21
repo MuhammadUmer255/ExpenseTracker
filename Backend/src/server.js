382766f74a365246a58c9a62e91fb7ffd65300f7
Muhammad Umer <muhammad.umer25052@gmail.com>
2026-07-21 21:40:12 +0500

    initial commit

---
+++ Backend/src/server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+const PORT = 5000;
+
+app.get("/", (req, res) => {
+    res.send("Expense Tracker API is Running...");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
